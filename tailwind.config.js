828be779770cf15013d3fe01bbc4014b30b9e30b
Edson <edsonp357@gmail.com>
2026-02-18 20:45:31 -0300

    Atualizando Tailwind config para incluir todos os diretórios

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./main.tsx",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./api/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
